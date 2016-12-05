648a893e7eb67184f98953a1165e29d51b46df66
Jed Frey <jedediahfrey@users.noreply.github.com>
2016-12-05 08:46:56 -0500

    Git Auto Commit: Mon Dec  5 08:46:56 EST 2016

---
+++ Start_Classifier/import_images.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import h5py
+from PIL import Image
+import numpy as np
+import os
+import glob
+
+
+image_list = list()
+classification_list = list()
+
+taxonomies = glob.glob("Images/*")
+for i in range(len(taxonomies)):
+    taxonomy = taxonomies[i]
+    
+    classification = np.zeros(len(taxonomies))
+    classification[i]=1.0
+        
+    for image in glob.glob(os.path.join(taxonomy,"*.png")):
+        image_ = Image.open(image)
+        image_list.append(np.asarray(image_)/255)
+        classification_list.append(classification)
+assert len(image_list)==len(classification_list)
+hdf_dataset = "imported_dataset.hdf5"
+
+opts=dict()
+opts["compression"]="gzip"
+opts["compression_opts"]=9
+with h5py.File(hdf_dataset, "w") as fid:
+    fid.create_dataset("images", data=image_list, **opts)
+    fid.create_dataset("classifications", data=classification_list, **opts)
