4ae1a57fe18c9d8ba5f8b6dbb59516b11f10bc62
Jed Frey <jedediahfrey@users.noreply.github.com>
2016-12-05 17:17:48 -0500

    Git Auto Commit: Mon Dec  5 17:17:48 EST 2016

--- Start_Classifier/import_images.py
+++ Start_Classifier/import_images.py
@@ -7,7 +7,6 @@ import numpy as np
 import os
 import glob
 
-
 image_list = list()
 classification_list = list()
 
@@ -20,7 +19,7 @@ for i in range(len(taxonomies)):
         
     for image in glob.glob(os.path.join(taxonomy,"*.jpg")):
         image_ = Image.open(image)
-        image_.thumbnail((32,32))
+        image_.resize((32,32))
         image_array = np.asarray(image_)/255
         assert(image_array.shape == (32, 32, 3))        
         classification_list.append(classification)
